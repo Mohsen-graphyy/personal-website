1ba58bb455fff0b20b1967b6bce2418e2065280d
mohsen_graphyy <piri.mohsen@ut.ac.ir>
2023-06-05 03:15:45 +0330

    Deploy to GitHub Pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,12 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
+  mode: 'spa',
+  generate: {
+    dir: 'docs'
+  },
+  router: {
+    base: '/personal-website/'
+  },
   layout: 'default',
   head: {
     title: 'resume',

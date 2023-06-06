7a97ad91013e1125ff5e69c5c4f8a7b5672a0071
mohsen.piri <mohsen@maktabkhooneh.org>
2023-06-06 04:13:19 +0330

    ✨ add hamerger menu and make responsive

---
+++ components/header/mobile/MobileHeader.vue
@@ -0,0 +1,48 @@
+<template>
+  <nav class="relative">
+    <div class="fixed top-0 right-0 w-full">
+      <div
+        class="flex justify-between flex-row py-5 px-2 items-center bg-white"
+      >
+        <div class="flex">
+          <nuxt-link :to="{ name: 'homePage' }"
+            ><img
+              class="w-10"
+              src="/personal-website/images/perp-MP-logo.png"
+              alt="Mohsen piri"
+          /></nuxt-link>
+          <base-button class="group ml-2" button-title="Job Offer!">
+            <svg
+              class="w-6 h-6 ml-3 group-hover:text-yellow-300 group-hover:animate-bounce text-violet-600 rotate-45"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+            >
+              <path
+                d="M5.85 3.5a.75.75 0 00-1.117-1 9.719 9.719 0 00-2.348 4.876.75.75 0 001.479.248A8.219 8.219 0 015.85 3.5zM19.267 2.5a.75.75 0 10-1.118 1 8.22 8.22 0 011.987 4.124.75.75 0 001.48-.248A9.72 9.72 0 0019.266 2.5z"
+              />
+              <path
+                fill-rule="evenodd"
+                d="M12 2.25A6.75 6.75 0 005.25 9v.75a8.217 8.217 0 01-2.119 5.52.75.75 0 00.298 1.206c1.544.57 3.16.99 4.831 1.243a3.75 3.75 0 107.48 0 24.583 24.583 0 004.83-1.244.75.75 0 00.298-1.205 8.217 8.217 0 01-2.118-5.52V9A6.75 6.75 0 0012 2.25zM9.75 18c0-.034 0-.067.002-.1a25.05 25.05 0 004.496 0l.002.1a2.25 2.25 0 11-4.5 0z"
+                clip-rule="evenodd"
+              />
+            </svg>
+          </base-button>
+        </div>
+        <HamburgerMenu />
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+import HamburgerMenu from "~/components/header/mobile/HambergerMenu.vue";
+import BaseButton from "~/components/base/BaseButton.vue";
+
+export default {
+  components: {
+    HamburgerMenu,
+    BaseButton,
+  },
+};
+</script>
